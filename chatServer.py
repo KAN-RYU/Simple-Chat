4c140517557fdac5cc212a3cdc558e1c3cef98d7
KAN-RYU <kanryu2048@unist.ac.kr>
2020-05-22 17:39:48 +0900

    broadcast message with Queue()

--- chatServer.py
+++ chatServer.py
@@ -1,17 +1,19 @@
 from socket import *
 import threading
+from queue import Queue
 import time
 
+messageQ = Queue()
 
 def send(sock):
     while True:
-        sendData = input('>>>')
-        sock.send(sendData.encode('utf-8'))
-
+        message = messageQ.get()
+        sock.send(message)
 
 def receive(sock):
     while True:
         recvData = sock.recv(1024)
+        messageQ.put(recvData)
         print('상대방 :', recvData.decode('utf-8'))
 
 
@@ -19,13 +21,13 @@ port = 8081
 
 serverSock = socket(AF_INET, SOCK_STREAM)
 serverSock.bind(('', port))
-serverSock.listen(1)
+serverSock.listen()
 
-print('%d번 포트로 접속 대기중...'%port)
+print('Wait connection from port %d'%port)
 
 connectionSock, addr = serverSock.accept()
 
-print(str(addr), '에서 접속되었습니다.')
+print('Connected from', str(addr))
 
 sender = threading.Thread(target=send, args=(connectionSock,))
 receiver = threading.Thread(target=receive, args=(connectionSock,))
